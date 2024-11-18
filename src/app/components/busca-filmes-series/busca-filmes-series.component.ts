8acfc87e0804d9ddf19a7a509200406ac3a46613
RenanAUG <amc.renan@gmail.com>
2024-11-18 19:30:50 -0300

    Adicionando validação de requisições ultrapassadas
    Mensagem para exibir quando ultrapassar 1 mil requisições por dia.

--- src/app/components/busca-filmes-series/busca-filmes-series.component.ts
+++ src/app/components/busca-filmes-series/busca-filmes-series.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FilmesFavoritosComponent } from './filmes-favoritos/filmes-favoritos.component';
 import {CardFilmesSeriesComponent} from './card-filmes-series/card-filmes-series.component';
+import {tap} from 'rxjs';
 
 @Component({
   selector: 'app-busca-filmes-series',
@@ -19,21 +20,41 @@ export class BuscaFilmesSeriesComponent {
   filmesFavoritos: FilmesSeriesModel[] = [];
   pesquisaRealizada: boolean = false;
   mensagemErro = false ;
+  mensagemErroRequisicoes = false;
+  quantRequisicoes = 0;
+  ultimaDataRequisicao = new Date();
 
   constructor(private filmeAndSeriesService: FilmesSeriesService) {
   }
 
   // Método de busca de filmes e séries
   buscarFilmesSeries() {
+    const dataAtual = new Date();
+
     if (!this.titulo.trim()) {
       this.resultados = [];
       this.pesquisaRealizada = false;
       return;
     }
 
+    // Verifica se é um novo dia
+    if (this.ultimaDataRequisicao.toDateString() !== dataAtual.toDateString()) {
+      this.quantRequisicoes = 0;
+      this.ultimaDataRequisicao = dataAtual;
+      this.mensagemErroRequisicoes = false;
+    }
+
     this.filmeAndSeriesService
       .buscaFilmesSeries(this.titulo)
-      .pipe()
+      .pipe(
+        tap(() => {
+          this.quantRequisicoes += 1;
+
+          if (this.quantRequisicoes > 1000) {
+            this.mensagemErroRequisicoes = true;
+          }
+        })
+      )
       .subscribe({
         next: (res) => {
           console.log(res);
